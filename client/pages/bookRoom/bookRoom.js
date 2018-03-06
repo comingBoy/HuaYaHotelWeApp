14455c03adab456267965a30919ec7e429f14211
LiLinFeng <594244263@qq.com>
2018-03-06 19:17:04 +0800

    房间详情组件实现

--- client/pages/bookRoom/bookRoom.js
+++ client/pages/bookRoom/bookRoom.js
@@ -7,8 +7,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    roomList:[
+    roomList: [
       {
+        album: [
+          "../../images/businessBed1.jpg",
+          "../../images/businessBed2.jpg",
+          "../../images/businessBed3.jpg",
+        ],
         roomType: "商务大床房",
         area: "25㎡",
         bedType: "大床",
@@ -17,7 +22,8 @@ Page({
         internet: "WiFi",
         bathroom: "独立",
         peopleNum: "2人",
-        楼层: "7-9层",
+        floor: "7-9层",
+        beChoosed: false,
       },
       {
         roomType: "商务标间",
@@ -28,9 +34,12 @@ Page({
         internet: "WiFi",
         bathroom: "独立",
         peopleNum: "2人",
-        楼层: "7-9层",
+        floor: "7-9层",
+        beChoosed: false,
       }
     ],
+    hiddenRoomDetail: true,
+    roomBeShowed: null,
     hotelAlbum: [],
     albumUrl: []
   },
@@ -75,49 +84,49 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
   toHotelAlbum: function () {
@@ -131,5 +140,113 @@ Page({
       current: '',
       urls: this.data.albumUrl
     })
+  },
+  /**
+   * 开始点击房间列表
+   */
+  touchRoomStart: function (e) {
+    var index = e.currentTarget.id
+    var roomList = this.data.roomList
+    roomList[index].beChoosed = true
+    this.setData({
+      roomList: roomList
+    })
+  },
+  /**
+   * 点击房间列表动作结束
+   */
+  touchRoomEnd: function (e) {
+    var index = e.currentTarget.id
+    var roomList = this.data.roomList
+    roomList[index].beChoosed = false
+    this.setData({
+      roomList: roomList
+    })
+  },
+  /**
+   * 点击房间列表后移动
+   */
+  touchRoomMove: function (e) {
+    var index = e.currentTarget.id
+    var roomList = this.data.roomList
+    roomList[index].beChoosed = false
+    this.setData({
+      roomList: roomList
+    })
+  },
+  /**
+   * 点击查看房间详情
+   */
+  showRoomDetail: function (e) {
+    var index = e.currentTarget.id
+    var roomBeShowed = this.data.roomList[index]
+    var hiddenRoomDetail = false
+
+    //第1步：创建动画实例
+    var animation = wx.createAnimation({
+      duration: 200,
+      transformOrigin: '50% 100% 0'
+    })
+
+    //第2步：这个动画实例赋给当前动画实例
+    this.animation = animation
+
+    //第3步：执行第一组动画
+    animation.opacity(0).scaleY(0).step();
+
+    // 第4步：导出动画对象赋给数据对象储存 
+    this.setData({
+      showRoomDetailAnimationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画 
+    setTimeout(function () {
+      // 执行第二组动画 
+      animation.opacity(1).scaleY(1).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象 
+      this.setData({
+        showRoomDetailAnimationData: animation
+      })
+    }.bind(this), 200)
+    this.setData({
+      roomBeShowed,
+      hiddenRoomDetail,
+    })
+  },
+  /**
+   * 隐藏房间详情界面
+   */
+  hiddenRoomDetail: function () {
+    var hiddenRoomDetail = true
+    //第1步：创建动画实例
+    var animation = wx.createAnimation({
+      duration: 200,
+      transformOrigin: '50% 100% 0'
+    })
+
+    //第2步：这个动画实例赋给当前动画实例
+    this.animation = animation
+
+    //第3步：执行第一组动画
+    animation.opacity(0).scaleY(0).step();
+
+    // 第4步：导出动画对象赋给数据对象储存 
+    this.setData({
+      showRoomDetailAnimationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画 
+    setTimeout(function () {
+      // 执行第二组动画 
+      animation.opacity(1).scaleY(1).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象 
+      this.setData({
+        showRoomDetailAnimationData: animation
+      })
+      this.setData({
+        hiddenRoomDetail
+      })
+    }.bind(this), 200)
+
   }
 })
