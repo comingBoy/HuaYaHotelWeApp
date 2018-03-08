746557ac199975f4f5bb7b0c323a137445489d6f
xinyuchen <1503750106@qq.com>
2018-03-08 16:14:09 +0800

    预订界面

--- client/pages/submitOrder/submitOrder.js
+++ client/pages/submitOrder/submitOrder.js
@@ -8,6 +8,9 @@ Page({
   data: {
     hiddenInTime: false,
     hiddenOverTime: false,
+    roomNum: ['一间', '两间', '三间', '四间', '五间', '六间', '七间', '八间', '九间', '十间'],
+    roomIndex: 0,
+    customerName: ["每间需要填写一人姓名"]
   },
 
   /**
@@ -82,5 +85,37 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * 房间数选择
+   */
+  chooseRoomNum: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    var num1 = parseInt(e.detail.value) + 1
+    console.log(num1)
+    var customerNameArray = new Array(num1)
+    console.log(customerNameArray.length)
+    for (var i=0; i< customerNameArray.length;i++){
+      customerNameArray[i] = "每间需要填写一人姓名"
+    }
+    console.log(customerNameArray)
+    this.setData({
+      roomIndex: e.detail.value,
+      customerName: customerNameArray
+    })
+  },
+/**
+ * 输入房间入住人
+ */
+  enterNmae:function(e){
+    console.log(e.currentTarget.id)
+    var customerNameArray = this.data.customerName
+    console.log(customerNameArray.length)
+    customerNameArray[e.currentTarget.id] = e.detail.value
+    console.log(customerNameArray.length)
+    this.setData({
+      customerName: customerNameArray
+    })
+    console.log(customerNameArray.length)
   }
 })
