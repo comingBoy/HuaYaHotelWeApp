e5c77cfed2312f53438a9878ccad26c94c944bcd
Xv Tao <1099869716@qq.com>
2018-03-13 14:35:52 +0800

    更换小程序

--- server/config.js
+++ server/config.js
@@ -17,11 +17,11 @@ const CONF = {
      * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
      */
     mysql: {
-        host: 'gz-cdb-95jq70nn.sql.tencentcdb.com:63053',
+        host: 'localhost',
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'huayahotel001huayahotel001',
+        pass: 'wxaf8344b6f294ff2d',
         char: 'utf8mb4'
     },
 
